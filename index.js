2a2331ed93c00aaa5c6b345252c598ec6947479a
twinh <twinhuang@qq.com>
2020-08-14 17:12:33 +0800

    style(eslint): single quotes, double jsx-quotes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // From https://github.com/ant-design/ant-design/blob/master/components/modal
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {Button, Modal} from "react-bootstrap";
+import {Button, Modal} from 'react-bootstrap';
 
 class ActionButton extends React.Component {
   timeoutId;
